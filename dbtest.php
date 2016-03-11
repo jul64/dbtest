5477b911a61debc55a1ea0338e3faa25350f4484
jul64 <julie.sehedic@ca-ts.fr>
2016-03-11 10:54:02 +0100

    Update dbtest.php

--- dbtest.php
+++ dbtest.php
@@ -1,12 +1,12 @@
 <?php
 $dbhost = getenv("MYSQL_SERVICE_HOST");
 $dbport = getenv("MYSQL_SERVICE_PORT");
-$dbuser = getenv("DATABASE_USER");
-$dbname = getenv("DATABASE_NAME");
-$dbpwd = getenv("DATABASE_PASSWORD");
+$dbuser = getenv("MYSQL_USER");
+$dbname = getenv("MYSQL_DATABASE");
+$dbpwd = getenv("MYSQL_PASSWORD");
  
 $connection = mysqli_connect($dbhost.":".$dbport, $dbuser, $dbpwd, $dbname) or die("Error " . mysqli_error($connection));
-$query = "SELECT * from users" or die("Error in the consult.." . mysqli_error($connection));
+$query = "SELECT * from user" or die("Error in the consult.." . mysqli_error($connection));
 echo "Hello All.. Here is the list of users: <br>";
 $rs = $connection->query($query);
 while ($row = mysqli_fetch_assoc($rs)) {
