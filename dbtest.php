722db436ae97555192f0e5e59c125ff07c149e01
jul64 <julie.sehedic@ca-ts.fr>
2016-04-19 09:20:56 +0200

    update nom db

--- dbtest.php
+++ dbtest.php
@@ -6,7 +6,7 @@ $dbname = getenv("MYSQL_DATABASE");
 $dbpwd = getenv("MYSQL_PASSWORD");
  
 $connection = mysqli_connect($dbhost.":".$dbport, $dbuser, $dbpwd, $dbname) or die("Error " . mysqli_error($connection));
-$query = "SELECT * from user" or die("Error in the consult.." . mysqli_error($connection));
+$query = "SELECT * from sampledb" or die("Error in the consult.." . mysqli_error($connection));
 echo "Hello All.. Here is the list of users: <br>";
 $rs = $connection->query($query);
 while ($row = mysqli_fetch_assoc($rs)) {
