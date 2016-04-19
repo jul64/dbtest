6c5b5f4a84954477ea3fda08e4f5cfc3be15e000
jul64 <julie.sehedic@ca-ts.fr>
2016-04-19 14:25:09 +0200

    change table name

--- dbtest.php
+++ dbtest.php
@@ -6,7 +6,7 @@ $dbname = getenv("MYSQL_DATABASE");
 $dbpwd = getenv("MYSQL_ROOT_PASSWORD");
  
 $connection = mysqli_connect($dbhost.":".$dbport, root, $dbpwd, $dbname) or die("Error " . mysqli_error($connection));
-$query = "SELECT * from sampledb" or die("Error in the consult.." . mysqli_error($connection));
+$query = "SELECT * from user" or die("Error in the consult.." . mysqli_error($connection));
 echo "Hello All.. Here is the list of users: <br>";
 $rs = $connection->query($query);
 while ($row = mysqli_fetch_assoc($rs)) {
